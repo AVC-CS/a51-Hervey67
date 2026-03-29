bd0514ea3732fb95b650b24ce6e62245450091b3
Hervey67 <herv2007@gmail.com>
2026-03-29 01:50:07 -0700

    Changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,23 @@ using namespace std;
 int main()
 {
     // TODO: declare variable n for the exponent input
-
+    double n, num;
+    cin >> n; 
+    num = 1.0;
+    
+    if (n >= 0){
+        for (int i = 0; i < n; i++){
+            num = num * 2;
+        }
+    }
+    else{
+        for (int i = n; i < 0; i++){
+            num = num * 2;
+        }
+        num = 1 / num;
+    }
+    cout << setprecision(5) << fixed; 
+    cout << "2 to Power " << n << " is " << num << endl;
     // TODO: read n from standard input
 
     // TODO: compute 2 to the power of n using a loop (no cmath pow)
